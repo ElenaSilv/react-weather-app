48ea27daf8c048a92216d086fc7d57d44bdabb98
ElenaSilv <elena.silvestricecinelli@gmail.com>
2022-04-20 19:20:08 +0100

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>What's the weather like?</h1>
+      <footer>
+        <small id="me">
+          <a href="https://github.com/ElenaSilv/Weather-app">Open-source</a>{" "}
+          project coded by Elena Silvestri Cecinelli
+        </small>
+      </footer>
     </div>
   );
 }
-
-export default App;
